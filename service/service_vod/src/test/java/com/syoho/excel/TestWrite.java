e7f899c07d39e3945b486bb16656b8149587c8b4
syoho <syoho.xiaofan@gmail.com>
2022-09-18 23:22:42 +0800

    Modify.

---
+++ service/service_vod/src/test/java/com/syoho/excel/TestWrite.java
@@ -0,0 +1,10 @@
+package com.syoho.excel;
+
+public class TestWrite {
+
+    public static void main(String[] args) {
+        //设置文件名和路径
+
+    }
+
+}
---
+++ service/service_vod/src/test/java/com/syoho/excel/User.java
@@ -0,0 +1,14 @@
+package com.syoho.excel;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.Data;
+
+@Data
+public class User {
+
+    @ExcelProperty(value = "用户编号",index = 0) //对应第一列
+    private int id;
+
+    @ExcelProperty(value = "用户名称",index = 1)//对应第二列
+    private String name;
+}
